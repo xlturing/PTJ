903d4810e55ddf2cb575da8c46fdbba396bc22f7
xlturing <litaoxiao@gmail.com>
2014-08-21 14:58:22 +0800

    C# console drink vending system_vs2010

---
+++ PTJ_22_C# Drink vending system_console application/code/PTJ_22/Display.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PTJ_22
+{
+    /* This is the view of our system */
+    class Display
+    {
+        /* Show the system main menu according to the option */
+        public static void ShowMenu(int option)
+        {
+            switch (option)
+            {
+                case 0:
+                    Console.WriteLine("Welcome to Ezi Drink AVM");
+                    break;
+                case 1:
+                    Console.WriteLine("Please choose");
+                    break;
+                case 2:
+                    Console.WriteLine("You can choose to collect your drink or obtain a refund of your coins");
+                    break;
+            }
+            
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("      1.Select Drink");
+            Console.WriteLine("      2.Insert Coins");
+            Console.WriteLine("      3.Refund Coins");
+            Console.WriteLine("      4.Collect Drink");
+            Console.WriteLine("      5.Exit Simulation\n");
+            Console.Write("Please enter your choice<1-5>:");
+        }
+
+        /* Show the dinks which the system have */
+        public static void ShowDrink()
+        {
+            Console.WriteLine("1.Soft Drink");
+            Console.WriteLine("2.Milk Dink");
+            Console.WriteLine("3.Fruit Dink");
+            Console.WriteLine("4.Cancel Selection\n");
+            Console.Write("Please select one type of drink<1-4>");
+        }
+
+        /* Show the costs of the drink which the user choose */
+        public static double ShowCost(int option)
+        {
+            Console.Write("Your drink costs ");
+            double cost;
+            switch (option)
+            {
+                case 1: 
+                    Console.WriteLine("$1.50");
+                    cost = 1.5;
+                    break;
+                case 2: 
+                    Console.WriteLine("$3.00");
+                    cost = 3.0;
+                    break;
+                default:
+                    Console.WriteLine("$2.20");
+                    cost = 2.20;
+                    break;
+            }
+
+            return cost;
+        }
+
+        /* Show the denominations of acceptable coins and show balance of cost */
+        public static void ShowCoin()
+        {
+            Console.WriteLine("2.0 = $2.00");
+            Console.WriteLine("1.0 = $1.00");
+            Console.WriteLine("0.5 = $0.50");
+            Console.WriteLine("0.2 = $0.20");
+            Console.WriteLine("0.1 = $0.10\n");
+        }
+
+        /* Enter a coin */
+        public static void ShowEnterCoin()
+        {
+            Console.Write("Please enter a coin value or 0 to Cancel: ");
+        }
+
+        /* Show balance costs */
+        public static void ShowBalanceCost(double balanceCost)
+        {
+            Console.WriteLine("Balance of cost ${0:F2}\n", balanceCost);
+        }
+
+        /* Show refund coins */
+        public static void ShowRefund(double amountEntered)
+        {
+            if(amountEntered != 0)
+                Console.WriteLine("Please collect your change of ${0:F2}\n", amountEntered);
+            else
+                Console.WriteLine("Sorry! You have not entered any coins\n");
+        }
+
+        /* Show the message of collect drinks */
+        public static void ShowCollectDrink()
+        {
+            Console.WriteLine("Please collect your drink. Enjoy.\n");
+        }
+
+        public static void ExitSystem()
+        {
+            Console.WriteLine("Thank you for using Ezi Drink AVM\n\n");
+            Console.WriteLine("Press any key to terminate simulation ...");
+        }
+
+        /* If user don't select a drink but choose other item, show this message */
+        public static void ShowErrSelected()
+        {
+            Console.WriteLine("Please select a drink first!\n");
+        }
+
+        /* If user input wrong denominations of coins, warn he/she */
+        public static void ShowErrDenominations()
+        {
+            Console.WriteLine("Please enter right denominations of acceptable coins!\n");
+        }
+
+        /* If user don't input enough coins to collect the drinks, warn he/she */
+        public static void ShowErrBalance()
+        {
+            Console.WriteLine("Sorry! You have not yet paid for this drink\n");
+        }
+
+        /* If user choose a wrong option, warn he or she */
+        public static void ShowErrOption()
+        {
+            Console.WriteLine("Please enter right Option!\n");
+        }
+    }
+}
---
+++ PTJ_22_C# Drink vending system_console application/code/PTJ_22/VendingMachine.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PTJ_22
+{
+    class VendingMachine
+    {
+        public double amountEntered = 0;
+        public double drinkCost = 0;
+        public bool drinkSelected = false;  // The original selected state of the drink is false
+        public const int EXIT = 5;
+
+        /* Action: Option corresponds to a valid choice from the main menu, and the corresponding menu option is called */
+        public void PerformOption(int option)
+        {
+            if (option == EXIT)
+            {
+                Display.ExitSystem();
+                Console.Read();
+                System.Environment.Exit(0); // Close the console
+            }
+            else if (option == 1)
+            {
+                SelectProduct();
+                drinkSelected = true;
+            }
+            else
+            {
+                if (!drinkSelected)
+                {
+                    Display.ShowErrSelected();
+                    Display.ShowMenu(1);
+                }
+                else
+                {
+                    switch (option)
+                    {
+                        case 2:
+                            InsertCoin();
+                            break;
+                        case 3:
+                            RefundCoins();
+                            break;
+                        case 4:
+                            CollectProduct();
+                            break;
+                        default:    // invalid enter option
+                            Display.ShowErrOption();
+                            break;
+                    }
+                }
+
+            }
+        }
+
+        /* Action: User has selected a drink and the user enters coins to the value of the drink or the user decides to cancel inserting coins */
+        public void InsertCoin()
+        {
+            Display.ShowCoin();
+            Display.ShowBalanceCost(drinkCost - amountEntered);
+            while (drinkCost > amountEntered)
+            {
+                Display.ShowEnterCoin();
+                double enterCoin = double.Parse(Console.ReadLine());    // input the coin
+                if (enterCoin != 2.0 && enterCoin != 1.0 && enterCoin != 0.5 && enterCoin != 0.2 && enterCoin != 0.1)
+                {
+                    Display.ShowErrDenominations();
+                    continue;
+                }
+                if (enterCoin == 0) // If user input 0, he/she choose cancel, so break
+                    break;
+                amountEntered += enterCoin;
+                Display.ShowBalanceCost(drinkCost - amountEntered);
+            }
+            if(drinkCost<=amountEntered)
+                Display.ShowMenu(2);    // tell user can collect drink or refund all coins
+        }
+
+        /* Action: Returns a valid coin value or 0 */
+        public double GetCoin()
+        {
+            return amountEntered;
+        }
+
+        /* Action: User selects a product */
+        public void SelectProduct()
+        {
+            Display.ShowDrink();    // Show all type of drinks
+            int option = int.Parse(Console.ReadLine());
+            if (option < 1 || option > 4)
+            {
+                Display.ShowErrOption();
+                Display.ShowMenu(1);
+            }
+            else if (option == 4)
+            {
+                // cancel, then show main menu and return
+                Display.ShowMenu(1);
+                return;
+            }
+            else
+            {
+                double cost = Display.ShowCost(option);   // Show the cost of the choice drink
+                drinkCost += cost;      // add to the drink costs
+                Display.ShowMenu(1);    // Then show the main menu
+            }
+        }
+
+        /* Action: User obtains a refund or receives change. */
+        public void RefundCoins()
+        {
+            Display.ShowRefund(amountEntered);
+            amountEntered = 0;  // Clear the amout of the coins which user entered
+            Display.ShowMenu(1);
+        }
+
+        /* Action: User collects the drink */
+        public void CollectProduct()
+        {
+            if (amountEntered >= drinkCost)
+            {
+                Display.ShowCollectDrink();
+                amountEntered -= drinkCost;
+                if (amountEntered > 0)
+                    RefundCoins();
+                drinkCost = 0;
+                drinkSelected = false;
+            }
+            else
+            {
+                Display.ShowErrBalance();
+                Display.ShowMenu(1);    // show the menu again
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            // init the vending machine
+            VendingMachine vm = new VendingMachine();
+            Display.ShowMenu(0);
+            
+            while (true)
+            {
+                int option = 0; // Get user input choice
+                try
+                {
+                    option = int.Parse(Console.ReadLine());
+                    if (option > 5 || option < 1)
+                        throw new Exception();
+                    vm.PerformOption(option);
+                }
+                catch (Exception e)
+                {
+                    // if user input wrong, warn it
+                    Display.ShowErrOption();
+                    Display.ShowMenu(1);
+                    continue;
+                }
+            }
+        }
+    }
+}
